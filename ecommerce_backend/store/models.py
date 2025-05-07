37085b31f781876c5bf7e8509f4ac3d1e498c86f
nikostudy599 <nikostudy1@gmail.com>
2025-05-07 19:39:58 +0300

    Add files via upload

--- ecommerce_backend/store/models.py
+++ ecommerce_backend/store/models.py
@@ -1,10 +1,40 @@
 from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_url = models.URLField(blank=True)
+# Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+    
+
+class Order(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.SET_NULL)
+    date = models.DateField()
+
+
+class OrderField(models.Model):
+    order_id = models.ManyToOneRel(Order,on_delete=models.SET_NULL)
+    product_id = models.ForeignKey(Product,on_delete=models.CASCADE)
+    unitprice = models.FloatField()
+    amount = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category_id = models.ForeignKey(Category)
+    image_id = models.ManyToOneRel(ProductImage)
+    price = models.FloatField()
+    discount = models.FloatField(default=0)
+    stock = models.IntegerField()
+
+class ProductImage(models.Model):
+    product_id = models.ForeignKey(Product)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
