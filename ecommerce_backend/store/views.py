e4b36a994e50ad71b2ef97a253e6dfced64f817e
Suraj Mishra <mishrasur7@gmail.com>
2025-05-15 09:39:50 +0300

    get single product added

--- ecommerce_backend/store/views.py
+++ ecommerce_backend/store/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -15,5 +17,13 @@ class ProductListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_202_CREATED)
         return Response(serializer.error, status=status.HTTP_404_BAD_REQUEST)
+    
+
+class SingleProductAPIView(APIView):
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
     
 
