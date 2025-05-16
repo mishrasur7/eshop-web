66b6a6810867fb0f50aebc65c2ec1ff918050f39
Suraj Mishra <mishrasur7@gmail.com>
2025-05-16 10:38:40 +0300

    view related to product moved to separate folder

--- ecommerce_backend/store/views/product_view.py
+++ ecommerce_backend/store/views/product_view.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
-from .models import Product
-from .serializers import ProductSerializer
+from store.models import Product
+from store.serializers import ProductSerializer
 
 class ProductListAPIView(APIView):
     # method to get all products
