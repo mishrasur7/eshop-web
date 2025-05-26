c2f94eb6a282beda56070aebca5268fbd927531f
rashid4807 <65720433+rashid4807@users.noreply.github.com>
2025-05-26 20:45:08 +0300

    Product details and similar product added.

--- ecommerce_frontend/src/App.js
+++ ecommerce_frontend/src/App.js
@@ -10,6 +10,7 @@ import ProductList from './components/ProductList';
 import AddProduct from './components/AddProduct';
 import LoginForm from './components/LoginForm';
 import RegisterationForm from './components/RegisterationForm';
+import ProductDetails from "./components/ProductDetails"
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginForm />} />
           <Route path='/register' element={<RegisterationForm />}/>
+          <Route path='/details' element={<ProductDetails />}/>
           <Route path='/' element={<ProductList />}/>
         </Routes>
       </main>
