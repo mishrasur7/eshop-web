d9194cb8247dc2ab5f232ffedf305ce1fe7cbc6f
rashid4807 <65720433+rashid4807@users.noreply.github.com>
2025-05-26 10:59:41 +0300

    Footer is modified

--- ecommerce_frontend/src/components/Footer.js
+++ ecommerce_frontend/src/components/Footer.js
@@ -1,16 +1,46 @@
 import React from 'react'
 import {  Container, Row, Col} from 'react-bootstrap'
+import logo from "../assets/images/logo.png";
+
+
 function Footer() {
-  return (
-     <footer>
-      <Container>
-        <Row>
-          <Col className='text-center py-3'>Copyright &copy; eshop-web</Col>
-        </Row>
-      </Container>
-     </footer>
- 
-  )
+    return (
+        <footer style={{ backgroundColor: "#fff", color: "black", paddingTop: "20px", paddingBottom: "20px" }}>
+            <Container>
+                <Row className="align-items-center">
+                    {/* Logo on the left */}
+                    <Col xs={12} md={4} className="text-center text-md-left">
+                        <img src={logo} alt="Logo" style={{ width: "100px", height: "100px" }} />
+                    </Col>
+
+                    {/* Contact Information in the middle */}
+                    <Col xs={12} md={4} className="text-center">
+                        <p>
+                            <strong>Contact Information:</strong><br />
+                            Address: Valimotie 00380 <br />00380 Helsinki, Finland<br /><br />
+                            Email: <a href="mailto:eshop-web@gmail.com" style={{ color: "#000" }}>eshop-web@gmail.com</a><br />
+                            Contact: +358XXXXXXXX
+                        </p>
+                    </Col>
+
+                    {/* Social Media Links on the right */}
+                    <Col xs={12} md={4} className="text-center text-md-right">
+                        <p>
+                            <strong>Follow us:</strong><br />
+                            
+                            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" style={{ color: "#000", marginRight: "10px" }}>Facebook</a>
+                            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" style={{ color: "#000", marginRight: "10px" }}>LinkedIn</a>
+                            <a href="https://wa.me/358417278258" target="_blank" rel="noopener noreferrer" style={{ color: "#000" }}>WhatsApp</a><br />
+
+                            <br />
+                            <p>&copy; copyright@eshop-web
+                            </p>
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
--- ecommerce_frontend/src/components/Header.js
+++ ecommerce_frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav, Container,Row } from 'react-bootstrap'
+import { Navbar, Nav, Container,NavDropdown} from 'react-bootstrap'
 function Header() {
   return (
     <header> 
@@ -9,9 +9,15 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-            <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+            
+          
           </Nav>
+
+          <Nav className='ms-auto'>
+            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+            <Nav.Link href="/login"><i className='fas fa-user'></i> Login</Nav.Link>
+          </Nav>
+
         </Navbar.Collapse>
         </Container>
       </Navbar>
