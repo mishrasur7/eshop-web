f411f735bf7491ff10e0241dc5f23e0b0ed1e055
Suraj Mishra <mishrasur7@gmail.com>
2025-05-27 09:20:42 +0300

    missing card component added

--- ecommerce_frontend/src/components/ProductList.jsx
+++ ecommerce_frontend/src/components/ProductList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col, } from 'react-bootstrap';
+import { Container, Row, Col, Card} from 'react-bootstrap';
 import axios from 'axios';
 
 import AddToCart from './AddToCart'
